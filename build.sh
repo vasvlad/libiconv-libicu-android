79fcf47ffa71b421ffc99cf4f1e7999557d54af1
Uladzislau Vasilyeu <vasvlad@gmail.com>
2018-03-28 18:59:05 +0300

    Adapted for build

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ cd $BUILDDIR/$ARCH
 
 # =========== libiconv.so ===========
 
-true || [ -e libiconv.so ] || {
+#true || [ -e libiconv.so ] || {
 
 	[ -e ../libiconv-1.14.tar.gz ] || curl -L http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz -o ../libiconv-1.14.tar.gz || exit 1
 
@@ -71,69 +71,7 @@ true || [ -e libiconv.so ] || {
 			sh -c '$STRIP'" $f.so"
 	done
 
-} || exit 1
-
-cd $BUILDDIR/$ARCH
-
-# =========== libicuXX.so ===========
-
-[ -e libicuuc$LIBSUFFIX.so ] || {
-
-	[ -e ../icu4c-55_1-src.tgz ] || exit 1
-
-	tar xvf ../icu4c-55_1-src.tgz
-
-	# The ENVVAR LIBSUFFIX should add the suffix only to the libname and not to the symbols.
-	# ToDo: Find the right way in Swift to refer to an alternative library with symbol prefixing or any other method to remove this.
-	if $[ -n "$LIBSUFFIX" ]; then
-		patch -p0 < ../patches/icu_suffix_only_on_libname.patch
-	fi
-
-	cd icu/source
-
-	#cp -f $BUILDDIR/config.sub .
-	#cp -f $BUILDDIR/config.guess .
-
-	[ -d cross ] || {
-		mkdir cross
-		cd cross
-		../configure || exit 1
-		make -j$NCPU VERBOSE=1 || exit 1
-		cd ..
-	} || exit 1
-
-
-	env CFLAGS="-I$NDK/sources/android/support/include -frtti -fexceptions" \
-		LDFLAGS="-frtti -fexceptions" \
-		LIBS="-L$BUILDDIR/$ARCH -landroid_support -lc++_shared -lstdc++" \
-		$BUILDDIR/setCrossEnvironment-$ARCH.sh \
-		./configure \
-		--host=arm-linux-androideabi \
-		--prefix=`pwd`/../../ \
-		--with-library-suffix=$LIBSUFFIX \
-		--with-cross-build=`pwd`/cross \
-		--enable-static --enable-shared \
-		|| exit 1
-
- 	sed -i.tmp 's/.$(SO_TARGET_VERSION_MAJOR)//' icudefs.mk
- 	sed -i.tmp 's/$(PKGDATA_VERSIONING) -e/-e/'  data/Makefile
-
-	env PATH=`pwd`:$PATH \
-		$BUILDDIR/setCrossEnvironment-$ARCH.sh \
-		make -j$NCPU VERBOSE=1 || exit 1
-
-	env PATH=`pwd`:$PATH \
-		$BUILDDIR/setCrossEnvironment-$ARCH.sh \
-		make V=1 install || exit 1
-
-	for f in libicudata$LIBSUFFIX libicutest$LIBSUFFIX libicui18n$LIBSUFFIX libicuio$LIBSUFFIX libicule$LIBSUFFIX libiculx$LIBSUFFIX libicutu$LIBSUFFIX libicuuc$LIBSUFFIX; do
-		cp -f -H ../../lib/$f.so ../../
-		cp -f ../../lib/$f.a ../../
-		$BUILDDIR/setCrossEnvironment-$ARCH.sh \
-			sh -c '$STRIP'" ../../$f.so"
-	done
-
-} || exit 1
+#} || exit 1
 
 done # for ARCH in armeabi armeabi-v7a
 
